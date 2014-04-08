8778432c279930f54fff29028b312d9035582368
work <cohimame@gmail.com>
2014-04-08 19:20:00 +0400

    new Action: find by regexp

--- app/controllers/Database.scala
+++ app/controllers/Database.scala
@@ -6,25 +6,44 @@ import play.api.libs.json._
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
 
+import reactivemongo.api.Cursor
+
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.Future
-import play.api.Logger
+
+import model.User
+import model.User._
 
 object Database extends Controller with MongoController {
   def users: JSONCollection = db.collection[JSONCollection]("users")
 
-
-
-
   def acceptUser = Action.async(parse.json) { request =>
     val json = request.body
+    val maybeValid = json.validate[User]
 
-    users.insert(json) map {
-      lastError => Created(s"Created with $lastError")
+    Logger info s"got $json body which was validated as $maybeValid"
+
+
+    maybeValid map { user =>
+      users.insert(user) map { lastError => Created(s"Created with $lastError") }
+    } getOrElse {
+      Future.successful(BadRequest("invalid json"))
     }
 
   }
 
+
+  def findUsers = Action.async {
+    val cursor: Cursor[User] = users.find(Json.obj("login" -> Json.obj("$regex" -> ".*"))).cursor[User]
+
+    val futureUsersList: Future[List[User]] = cursor.collect[List]()
+
+    val futurePersonsJsonArray = futureUsersList map { users => Json.arr(users) }
+
+    futurePersonsJsonArray map { users =>  Ok(users(0)) }
+  }
+
+  /*
   def findUser = Action.async(parse.json) { request =>
     import model.User
     import model.User._
@@ -47,5 +66,5 @@ object Database extends Controller with MongoController {
     }
 
   }
-
+    */
 }
